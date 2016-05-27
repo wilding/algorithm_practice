852e7c06bd6d42bdaa08428df583ea7b0b25a3a0
wilding <wildingthrelfall@gmail.com>
2016-05-27 13:10:55 -0700

    add Binary Search Tree Class
    - stores a root Node
    - defines the following methods:
    	insert(new_val)
    		inserts a new value into the appropriate place in the BST
    	search(find_val)
    		returns True if value is in the BST, False otherwise
    	print_tree()
    		prints the node values of the BST in pre-order traversal order
    	bst_search(start, find_val)
    		helper method - recursive search solution with logarithmic efficiency
    	bst_insert(start, new_val)
    		helper method - recursive insert solution with logarithmic efficiency
    	preorder_print(start, traversal)
    		helper method - recursive print solution with linear efficiency

--- binary_tree.py
+++ binary_tree.py
@@ -47,19 +47,68 @@ class BinaryTree(object):
         return traversal
 
 
-# Set up tree
-tree = BinaryTree(1)
-tree.root.left = Node(2)
-tree.root.right = Node(3)
-tree.root.left.left = Node(4)
-tree.root.left.right = Node(5)
+class BST(object):
+    def __init__(self, root):
+        self.root = Node(root)
 
-# Test search
-# Should be True
-print tree.search(4)
-# Should be False
-print tree.search(6)
+    def insert(self, new_val):
+        """inserts a new value into the BST
+        at the appropriate position"""
+        self.bst_insert(self.root, new_val)
+        pass
 
-# Test print_tree
-# Should be 1-2-4-5-3
-print tree.print_tree()
+    def search(self, find_val):
+        """Return True if the given value is in
+        the BST, False otherwise"""
+        return self.bst_search(self.root, find_val)
+
+    def print_tree(self):
+        """Print out all tree nodes
+        as they are visited in
+        a pre-order traversal."""
+        output = ""
+        traversal = self.preorder_print(self.root, [])
+        for value in traversal:
+            output += str(value) + "-"
+        output = output[:-1]
+        print output
+
+    def bst_search(self, start, find_val):
+        """Helper method - use this to create a
+        recursive search solution
+        with logarithmic efficiency"""
+        if start:
+            if find_val == start.value:
+                return True
+            elif find_val < start.value:
+                return self.bst_search(start.left, find_val)
+            elif find_val > start.value:
+                return self.bst_search(start.right, find_val)
+        return False
+
+    def bst_insert(self, start, new_val):
+        """Helper method - use this to create a
+        recursive insert solution"""
+        if start:
+            if new_val < start.value:
+                if start.left:
+                    self.bst_insert(start.left, new_val)
+                else:
+                    start.left = Node(new_val)
+            if new_val > start.value:
+                if start.right:
+                    self.bst_insert(start.right, new_val)
+                else:
+                    start.right = Node(new_val)
+        pass
+
+    def preorder_print(self, start, traversal):
+        """Helper method - use this to create a
+        recursive print solution."""
+        if start:
+            traversal.append(start.value)
+            if start.left:
+                traversal = self.preorder_print(start.left, traversal)
+            if start.right:
+                traversal = self.preorder_print(start.right, traversal)
+        return traversal
