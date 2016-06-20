cf38c2f7df7757621d88f07da032e0c0dc5082da
wilding <wildingthrelfall@gmail.com>
2016-06-20 11:25:22 -0700

    add docstrings for Graph class methods

--- graph.py
+++ graph.py
@@ -17,9 +17,15 @@ class Graph(object):
 		self.nodes = nodes
 		self.edges = edges
 	def insert_node(self, new_node_value):
+		"""creates a new node with the given value and
+		inserts it into the graph's list of nodes"""
 		new_node = Node(new_node_value)
 		self.nodes.append(new_node)
 	def insert_edge(self, new_edge_value, node_from_val, node_to_val):
+		"""creates a new edge with a given value between nodes with the specified values.
+		If no nodes exist with the given values, new nodes are created.
+		The new edge is then added to the graph's list of edges,
+		as well as the individual node's list of edges"""
 		from_found = None
 		to_found = None
 		for node in self.nodes:
@@ -38,6 +44,9 @@ class Graph(object):
 		to_found.edges.append(new_edge)
 		self.edges.append(new_edge)
 	def get_edge_list(self):
+		"""returns a list of tuples, each representing an edge,
+		that contain the edge value, the value of the origin node,
+		and the value of the destination node"""
 		edge_list = []
 		for edge in self.edges:
 			edge_list.append((edge.value, edge.node_from.value, edge.node_to.value))
