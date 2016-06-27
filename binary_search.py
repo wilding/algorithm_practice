0c94e55df26526f423d86611c23f9a5bcd5bb49d
wilding <wildingthrelfall@gmail.com>
2016-06-27 14:37:16 -0700

    add bubble_sort() function to binary_search.py
    bubble_sort(inputArray)
    	- sorts an array with O(n^2) efficiency
    	- compares adjacent elements in the list, and swaps them if necessary
    	- continues until full list is sorted
    
    longer binary_search() function removed

--- binary_search.py
+++ binary_search.py
@@ -1,28 +1,3 @@
-def binary_search(input_array, value):
-    """Same as index() but faster."""
-    counter = 0
-    while len(input_array) > 0:
-        middle = len(input_array) / 2
-        if value == input_array[middle]:
-            return middle + counter
-        elif value > input_array[middle]:
-            if len(input_array) == 1:
-                input_array = []
-            else:
-                counter += len(input_array[:middle])
-                input_array = input_array[middle:]
-        elif value < input_array[middle]:
-            if len(input_array) == 1:
-                input_array = []
-            else:
-                input_array = input_array[:middle]
-    return -1
-
-
-
-
-#################################   solution
-
 def binary_search(listData, value):
     low = 0
     high = len(listData) - 1
@@ -36,11 +11,34 @@ def binary_search(listData, value):
             high = mid + 1
     return -1
 
-
-
-
 def get_fib(position):
     if position == 0 or position == 1:
         return position
     else:
         return get_fib(position - 1) + get_fib(position - 2)
+
+#######   Sorting   #######
+
+def bubble_sort(inputArray):
+    iterations = 0
+    while iterations < len(inputArray):
+        for index, value in enumerate(inputArray):
+            sorted_point = len(inputArray) - 1 - iterations
+            if index < sorted_point and value > inputArray[index + 1]:
+                greater = value
+                lesser = inputArray[index + 1]
+                inputArray[index] = lesser
+                inputArray[index + 1] = greater
+        iterations += 1
+    return inputArray
+
+
+#def merge_sort():
+#def quick_sort():
+#def timsort():
+#def heap_sort():
+#def insertion_sort():
+#def selection_sort():
+#def shell_sort():
+#def bucket_sort():
+#def radix_sort():
