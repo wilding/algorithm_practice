eb95b16c68fcb3cc4cb7ddfaa60a1c2e69342143
wilding <wildingthrelfall@gmail.com>
2016-07-04 16:50:18 -0700

    add merge_sort.py
    defines the following functions:
    
        merge_sort(inputArray)
            takes an array and breaks it down into smaller subarrays
            sorts subarrays recursively
            merges subarrays
    
        merge(left, right)
            receives two sorted arrays as arguments
            merges the elements of the two arrays into a single sorted array

---
+++ merge_sort.py
@@ -0,0 +1,46 @@
+def merge_sort(inputArray):
+    if len(inputArray) <= 1:
+        return inputArray
+    left = []
+    right = []
+    for i in range(len(inputArray)):
+        value = inputArray[i]
+        if i % 2 == 0:
+            right.append(value)
+        else:
+            left.append(value)
+    left = merge_sort(left)
+    right = merge_sort(right)
+    if left[-1] <= right[0]:
+        return left + right
+    if right[-1] <= left[0]:
+        return right + left
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    while left and right:
+        if left[0] <= right[0]:
+            result.append(left[0])
+            left = left[1:]
+        else:
+            result.append(right[0])
+            right = right[1:]
+    while left:
+        result.append(left[0])
+        left = left[1:]
+    while right:
+        result.append(right[0])
+        right = right[1:]
+    return result
+
+
+array1 = [2, 4, 3, 5, 1]
+array2 = [6, 8, 9, 3, 12, 999, 32, 498, 901]
+array3 = [x for x in range(100)]
+array4 = [4, 73, 23, 41, 97, 444, 75, 0, -34, 88, 88, 90, 345, 79]
+print merge_sort(array1)
+print merge_sort(array2)
+print merge_sort(array3)
+print merge_sort(array4)
