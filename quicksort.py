d29e9f0e47edfd29f483babfbdddfa3a71ffe169
wilding <wildingthrelfall@gmail.com>
2016-07-05 22:19:54 -0700

    add in_place_quicksort() to quicksort.py
    rearranges elements of the input array and recursively sorts partitions of the input array
    rather than creating new arrays

--- quicksort.py
+++ quicksort.py
@@ -18,6 +18,30 @@ def quicksort(inputArray):
     return lesser + equal + [pivot] + greater
 
 
+def in_place_quicksort(inputArray):
+    if len(inputArray) <= 1:
+        return inputArray
+    pivot_index = len(inputArray) - 1
+    for index in range(len(inputArray)):
+        if index >= pivot_index:
+            break
+        while inputArray[index] > inputArray[pivot_index]:
+            value = inputArray[index]
+            pivot = inputArray[pivot_index]
+            if index == pivot_index - 1:
+                before = pivot
+            else:
+                before = inputArray[pivot_index - 1]
+            inputArray[pivot_index] = value
+            inputArray[pivot_index - 1] = pivot
+            inputArray[index] = before
+            pivot_index -= 1
+    inputArray[:pivot_index] = in_place_quicksort(inputArray[:pivot_index])
+    inputArray[pivot_index + 1:] = in_place_quicksort(inputArray[pivot_index + 1:])
+    # print inputArray
+    return inputArray
+
+
 array1 = [1, 2, 3, 4, 5]  # worst case - quadratic
 array2 = [2, 4, 1, 3, 5]  # avg case - linearithmic
 array3 = [2, 2, 2, 2, 2]  # best case - linear
