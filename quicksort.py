888f551d6b31a404b53461bd10d7e1170c91ed7e
wilding <wildingthrelfall@gmail.com>
2016-07-05 22:13:05 -0700

    optimize quicksort() for arrays with equal elements
    add elements with equal values to pivot to 'equal' array
    equal array does not need to be sorted, as all elements are same value
    add equal array to return statement before pivot to maintain stable sort

--- quicksort.py
+++ quicksort.py
@@ -4,19 +4,27 @@ def quicksort(inputArray):
     pivot = inputArray[-1]
     lesser = []
     greater = []
+    equal = []
     for value in inputArray[:-1]:
         if value > pivot:
             greater.append(value)
-        else:
+        elif value < pivot:
             lesser.append(value)
+        elif value == pivot:
+            equal.append(value)
     lesser = quicksort(lesser)
     greater = quicksort(greater)
-    return lesser + [pivot] + greater
+    # print inputArray
+    return lesser + equal + [pivot] + greater
 
 
-array1 = [1, 2, 3, 4, 5]  # worst case
-array2 = [2, 4, 1, 3, 5]
-array3 = [23, 44, 55, 2, 44, 30, 35, 11, -23, 40]
+array1 = [1, 2, 3, 4, 5]  # worst case - quadratic
+array2 = [2, 4, 1, 3, 5]  # avg case - linearithmic
+array3 = [2, 2, 2, 2, 2]  # best case - linear
+array4 = [2, 7, 6, 3, 5]
+array5 = [2, 7, 6, 8, 5]
 print quicksort(array1)
 print quicksort(array2)
 print quicksort(array3)
+print quicksort(array4)
+print quicksort(array5)
