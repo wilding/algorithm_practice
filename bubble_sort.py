fa7f6d361a4ac87b8618a24618e49c115eaf4324
wilding <wildingthrelfall@gmail.com>
2016-07-25 19:40:15 -0700

    docs: add docstring for bubble_sort()

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,18 @@
 def bubble_sort(inputArray):
+    """input: array
+    output: sorted array
+    features: in-place
+    efficiency: O(n^2) (worst/avg cases), O(n) (best case)
+    space complexity: O(1)
+    method:
+    Iterate through the list;
+    compare adjacent elements and swap when necessary.
+    Each full iteration, the largest element 'sinks' to end of list.
+    If no swaps are made in a full iteration,
+    the sorted list is immediately returned.
+    Each subsequent iteration only iterates through the subset of the list
+    before the index of the final swap from the last iteration.
+    """
     swap = True
     sorted_point = len(inputArray)
     new_point = None
