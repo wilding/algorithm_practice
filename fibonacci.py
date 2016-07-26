669c39d886d51cc9b2884216c64228559e5af012
wilding <wildingthrelfall@gmail.com>
2016-07-25 19:51:10 -0700

    add get_fib_list() function to fibonacci.py
        - takes an int representing an index as input
        - returns a list of the fibonacci sequence, starting at 0 and finishing at the given index
        - if input is negative, returns -1

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,27 @@
 def get_fib(position):
     """docstring for get_fib"""
+    if position < 0:
+        return -1
     if position == 0 or position == 1:
         return position
     else:
         return get_fib(position - 1) + get_fib(position - 2)
 
 
-fibarray = [get_fib(x) for x in range(21)]
-print fibarray
+def get_fib_list(position):
+    if position < 0:
+        return -1
+    else:
+        return [get_fib(x) for x in range(position + 1)]
+
+
+print get_fib(0)
+print get_fib(1)
+print get_fib(-1)
+print get_fib(2)
+print get_fib(5)
+print get_fib_list(0)
+print get_fib_list(1)
+print get_fib_list(-1)
+print get_fib_list(10)
+print get_fib_list(20)
