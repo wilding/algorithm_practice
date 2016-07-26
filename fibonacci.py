d8bfec6c6e35897615ccc2a0f0917bcdb0001c5c
wilding <wildingthrelfall@gmail.com>
2016-07-26 14:29:52 -0700

    refactor: refactor get_fib_list() to not use get_fib() function
        refactor testing

--- fibonacci.py
+++ fibonacci.py
@@ -9,19 +9,18 @@ def get_fib(position):
 
 
 def get_fib_list(position):
+    """docstring for get_fib_list"""
     if position < 0:
         return -1
+    if position == 0 or position == 1:
+        return [x for x in range(position + 1)]
     else:
-        return [get_fib(x) for x in range(position + 1)]
+        fiblist = get_fib_list(position - 1)
+        fiblist.append(fiblist[-1] + fiblist[-2])
+        return fiblist
 
 
-print get_fib(0)
-print get_fib(1)
 print get_fib(-1)
-print get_fib(2)
-print get_fib(5)
-print get_fib_list(0)
-print get_fib_list(1)
+print [get_fib(x) for x in range(31)]
 print get_fib_list(-1)
-print get_fib_list(10)
-print get_fib_list(20)
+print get_fib_list(30)
